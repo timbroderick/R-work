dacfff56949518a73dd805ee74d789ac70277e18
dailyherald <tbroderick@dailyherald.com>
2017-05-31 13:29:25 -0500

    starting ggplot2

--- R graphics/08_ggplotLines.R
+++ R graphics/08_ggplotLines.R
@@ -10,11 +10,38 @@ summary(df)
 library(ggplot2)
 library(ggthemes)
 
-p1 <- ggplot(df, aes(Total_RTR_incidents, UOF_only, color = factor(years)))
-p1 + geom_point() + geom_line()
+# need to make the data long, not wide
+# first grab the columns we want
+dfUOF <- subset(df, select = c("years","Year_Quarter","UOF_only"))
+dfSOF <- subset(df, select = c("years","Year_Quarter","SOF_only"))
+dfTRAN <- subset(df, select = c("years","Year_Quarter","Total_transitions"))
+# add columns for label purposes
+dfUOF$force <- "UOF_only"
+dfSOF$force <- "SOF_only"
+dfTRAN$force <- "Total_transitions"
+# now rename UOF SOF etc to common name
+names(dfUOF)[names(dfUOF)=="UOF_only"] <- "value"
+names(dfSOF)[names(dfSOF)=="SOF_only"] <- "value"
+names(dfTRAN)[names(dfTRAN)=="Total_transitions"] <- "value"
 
-p2 <- ggplot(df, aes(Total_RTR_incidents,UOF_only))
-p2 + geom_point() + stat_smooth() 
+# Now we can append the three data frames
+dfsub <- rbind(dfUOF,dfSOF,dfTRAN)
+dfsub
 
-p3 <- ggplot(df, aes(years,UOF_only)) + stat_summary(fun.y = median, geom = "bar")
-p3
+# stacked bars
+pBar <- ggplot(dfsub, aes(Year_Quarter, value, fill = force)) + 
+  geom_bar(position = "stack", stat="identity") + coord_flip()
+pBar
+
+# grouped bars
+pBar2 <- ggplot(dfsub, aes(Year_Quarter, value, fill = force)) + 
+  geom_bar(position = "dodge", stat="identity") + coord_flip()
+pBar2
+
+# line chart
+pLine <- ggplot(dfsub, aes(Year_Quarter, value, group = force, col = force))
+pLine + geom_point(stat="identity") + geom_line(stat="identity")
+
+p1 <- ggplot() + geom_line(aes(y = value, x = Year_Quarter, group = force, colour = force),
+                           data = dfsub, stat="identity")
+p1
