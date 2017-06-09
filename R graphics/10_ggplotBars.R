adcfe4351a6038950817376fe7170e030d948796
dailyherald <tbroderick@dailyherald.com>
2017-06-08 19:09:13 -0500

    more ggplot designing

--- R graphics/10_ggplotBars.R
+++ R graphics/10_ggplotBars.R
@@ -11,47 +11,59 @@ library(ggthemes)
 library(plyr)
 
 # basic bar chart
-basebar <- ggplot() + geom_bar(aes(x = Year_Quarter, y = Total_RTR_incidents, 
-                                   fill = factor(years) ), 
-                               data = dfrtr,
-                               stat="identity") + coord_flip()
+basebar <- ggplot(dfrtr) + 
+  geom_bar(aes(x = Year_Quarter, 
+               y = Total_RTR_incidents, 
+               fill = factor(years) ), 
+           stat="identity") + coord_flip()
 basebar
 
 # stacked bars
-stacked <- ggplot() +
-  geom_bar(data=df, 
-           aes(Year_Quarter, value, fill = force),
-           position = "stack", 
-           stat="identity") + coord_flip()
-stacked
-
-# grouped bars
-grouped <- ggplot() +
-  geom_bar(data=df, 
-           aes(Year_Quarter, value, fill = force),
-           position = "dodge", 
-           stat="identity") + coord_flip()
-grouped
-
-# create a pub ready version of stacked
-# and play with ggplot themes
-
-#----Set up plot for print and online --------
-#dev.cur()
-#pdf(file="ggplotBars.pdf", width = 4, height = 4) 
-#png(filename = "ggplotBars.png",width = 600, height = 400, units = "px")
-#-----Insert plot here -------------
-
-# stacked bars
-stacked <- ggplot() + geom_bar(data=df, 
-           aes(Year_Quarter, 
+stackedbar <- ggplot(df) +
+  geom_bar(aes(Year_Quarter, 
                value, 
                fill = force),
            position = "stack", 
            stat="identity") + coord_flip()
-#adjust legend position
-stacked <- stacked + theme(legend.position="top", legend.direction="horizontal",
-                  legend.title = element_blank())
+stackedbar
+
+# grouped bars
+grouped <- ggplot(df) +
+  geom_bar(aes(Year_Quarter, 
+               value, 
+               fill = force),
+           position = "dodge", 
+           stat="identity") + coord_flip()
+grouped
+
+# here we change the names in the data frame
+df$force <- factor(df$force, levels = c("Total_transitions","SOF_only","UOF_only"),
+                   labels = c("Transitions","Show of force","Use of force" ))
+
+
+#----Set up plot for print and online --------
+dev.cur()
+#pdf(file="ggplotBars.pdf", width = 6, height = 4.5) 
+png(filename = "ggplotBars.png",width = 600, height = 500, units = "px")
+#-----Insert plot here -------------
+
+# stacked bars
+stacked <- ggplot(df) + 
+  aes(x =Year_Quarter,
+      y = value, 
+      fill = force, 
+      label = value) + 
+  geom_bar(stat = "identity") + 
+  coord_flip() + theme_fivethirtyeight()
+
+#adjust legend position and change label names
+
+
+stacked <- stacked + 
+  theme(legend.position="top", 
+        legend.direction="horizontal",
+        legend.title = element_blank())
+
 
 # add titles
 stacked <- stacked + labs(
@@ -61,10 +73,17 @@ stacked <- stacked + labs(
   y="Types of response-to-resistance",
   caption="caption here")
 
-#df <- ddply(df, .(Year_Quarter), transform, pos = value) #cumsum(value) )#- (value * 0.10))
-#stacked <- stacked + geom_text(data=df, aes(x = Year_Quarter, y = value + (value*.5), label = value), size = 4)
+# add bar labels
+stacked <- stacked + geom_text(
+  position = "stack", 
+  #data=df, 
+  aes(x = Year_Quarter, 
+      y = value - (value * 0.025), 
+      hjust = 1,
+      label = value), 
+  size = 3.5)
 
 stacked
 
 #----- End plot --------------
-#dev.off() 
+dev.off() 
