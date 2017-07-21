fe0c8a7456205cb0b7979fbab7a9b5e229f50e6f
dailyherald <tbroderick@dailyherald.com>
2017-07-21 18:59:10 -0500

    adding markdown tutorials

--- R graphics/05_basicPlots1.R
+++ R graphics/05_basicPlots1.R
@@ -9,97 +9,44 @@ summary(df)
 # Now lets see this as a scatter plot
 with(df, plot(Total_RTR, UOF_only))
 title(main = "UOF vs Total RTR incidents")
-# What you're seeing here
-# The first line of code, starting "with", creates a scatter plot. 
-# First we say what dataset we want (elgCrime), then
-# what we want to test - Total RTR incidents vs UOF only
-# What it shows is that as there are more RTR incidents, 
-# the number of incidents that involve UOF only also grows.
-# Title obviously adds a title
 
-# We can add some other things to help us understand the data too
+# Add median lines
 abline(h=median(df$UOF_only), lty=2, lwd=2) 
-# adds horizontal line for median UOF incidents which looks to be about 19
 abline(v=median(df$Total_RTR), lwd=2, col="red") 
-# adds vertical red line for median RTR incidents, about 36
 
-
+# compute the regression or fit line and add to chart
 fit <- lm(df$UOF_only~df$Total_RTR)
-summary(fit)
-# Lots to uppack here. Honestly, I am not qualified to
-# explain in detail these more advanced statistic measures. 
-# What follows is a very over-simplified explanation. 
-# Look at the last three lines. Generally, you want ...
-# A) your Adjusted R-square to be as close to 1.00 as possible
-#    here it's 0.6556, which means it's estimated that 
-#    about 66 percent of the observations can be explained by
-#    the relationship between the two data set
-#    Which makes sense - the more Totol RTR incidents there are
-#    the more you see UOF_only incidents
-# B) the p-value to be less that 0.05
-
-# HOWEVER: Don't expect too much out of these stats - we're using a very 
-# small dataset so it's not going to be very informative and may even
-# cause confusing results. Look at the portion labeled Coefficients:
-
-# C) Std. Error and t value. You want the two t values to be as large 
-#    as possible versus the std errors. It is for one line 
-#     and not the other. It's telling us that with only 12 datapoints, 
-#    it's not able to make any definitive predictions. It just
-#    doesn't have enough data to tell whether any new points added
-#    would likely fall along the regression line.
-# D) And that confusion carries on into the Pr(>|t|) or p-value. 
-#    It uses those T values and Error to compute the P-value
-#    The top one is well over 0.05 which isn't good, but the bottom one 
-#    is well under so much so that R has tagged it with *** 
-#    indicating it's significant
-# ------
-# Statistics is a science. Our purpose in graphing these
-# values out is to see how the data relates to the
-# regression line and maybe spot outliers. We will seldom use 
-# T values or R2 in our stories.
-# But there's nothing wrong in knowing about these things. And in fact
-# understanding when it's telling you that something's wrong can
-# be very useful.
-# Here's a link to a resource explaining what you see with the summary
-# https://feliperego.github.io/blog/2015/10/23/Interpreting-Model-Output-In-R
-# If you want to learn more about statistics, let me know. There's a 
-# good textbook I can recommend.
-
-# add the regression line, or a line approximating the running average
 abline(fit)
 
-# Now, let's take a look at all three RTR categories
-# "Par" sets up the plot window to show three plots in 1 column (mfrow)
-# mar is margins
-par(mfrow=c(3,1),mar=c(4,4,2,1)) 
+# take a look at the fit stats
+summary(fit)
+
+# Seeing all three RTR categories
+
+# set up the plot area so that there's three plots
+par(mfrow=c(3,1),mar=c(4,4,2,1))
 
 # Let's add the plot we already did
-with(df, plot(Total_RTR, UOF_only)) # scatter plot
-title(main = "UOF vs Total RTR incidents") # adds title
+with(df, plot(Total_RTR, UOF_only,
+              xlim=c(0, 60), ylim=c(0, 35)))
+title(main = "UOF vs Total RTR incidents")
 abline(h=median(df$UOF_only), lty=2, lwd=2) 
 abline(v=median(df$Total_RTR), lwd=2, col="red") 
 abline(lm(df$UOF_only~df$Total_RTR))
 
 # now SOF 
-with(df, plot(Total_RTR, SOF_only)) # scatter plot
-title(main = "SOF vs Total RTR incidents") # adds title
+with(df, plot(Total_RTR, SOF_only,
+              xlim=c(0, 60), ylim=c(0, 35)))
+title(main = "SOF vs Total RTR incidents")
 abline(h=median(df$SOF_only), lty=2, lwd=2) 
 abline(v=median(df$Total_RTR), lwd=2, col="red") 
 abline(lm(df$SOF_only~df$Total_RTR))
 
 # finally, transitions
-with(df, plot(Total_RTR, Transitions)) # scatter plot
-title(main = "Transitions vs Total RTR incidents") # adds title
+with(df, plot(Total_RTR, Transitions,
+              xlim=c(0, 60), ylim=c(0, 35))) 
+title(main = "Transitions vs Total RTR incidents")
 abline(h=median(df$Transitions), lty=2, lwd=2) 
 abline(v=median(df$Total_RTR), lwd=2, col="red") 
 abline(lm(df$Transitions~df$Total_RTR))
 
-# You may need to make your plot window larger to see the results
-# again, not enough data for any insights but it does appear
-# that while all RTR increase about the same rate,
-# there are not only more UOF incidents per quarter but
-# there are more quarters with higher-than-average UOF incidents
-# than SOF or transitions.
-
-# there's a better way of doing multiple plots that we'll look at later
--- R graphics/06_basicPlots2.R
+++ R graphics/06_basicPlots2.R
@@ -1,61 +1,61 @@
-# Continuing to work with the RTR data
+# The code in this file is described 
+# in the pdf and html files of the same name
+
 library(readr)
 df <- read_csv("dfCrime.csv")
 
-# notice we loaded in the data with the years column
-# Let's get the colummn names and summary data
-names(df)
 summary(df)
 
-
-# Now I want to try and highlight the values in the scatter plot by their years
-# We start with a basic plot, adding all the titles and labels here
+# create basic plot
 plot(df$Total_RTR, df$UOF_only, 
+     xlim=c(0, 60), ylim=c(0, 35),
      main = "UOF vs Total RTR incidents", 
-     # sub = "Daily Herald graphic", 
      xlab="Total response to resistance incidents", 
      ylab="Use of force incidents only")
-# Now we're going to subset the data by the year 2014
-y2014 <- subset(df, year==2014)
-# And add that subset to the plot, with the color of red and
-# as a solid cirlce (pch=19)
-points(y2014$Total_RTR,y2014$UOF_only,col="red",pch=19)
-# to learn more, don't forget you can use Rstudio's built in query tool
-# simply type ?points 
 
-# Now we do the same with the other two years
+# Select data out of total for each year
+y2014 <- subset(df, year==2014)
 y2015 <- subset(df, year==2015)
-points(y2015$Total_RTR,y2015$UOF_only,col="blue",pch=19)
 y2016 <- subset(df, year==2016)
+# Add those to plot
+points(y2014$Total_RTR,y2014$UOF_only,col="red",pch=19)
+points(y2015$Total_RTR,y2015$UOF_only,col="blue",pch=19)
 points(y2016$Total_RTR,y2016$UOF_only,col="green",pch=19)
 
-# We also need to add a legend to the plot
-legend("topleft", pch = c(19,19,19), col = c("red","blue","green"), legend = c("2014", "2015", "2016"))
-# Note that legends can be tricky. You have to leave room for them so they
-# don't obscure the data. That means you may need to adjust the scales of your
-# graphic beyond what it defaults to. Not something we need to worry about now
+# Add a legend
+legend("topleft", 
+       pch = c(19,19,19), 
+       col = c("red","blue","green"), 
+       legend = c("2014", "2015", "2016"))
 
-# Let's add the regression line
+# Add the regression line
 abline(lm(df$UOF_only~df$Total_RTR))
 
-# so now we can see not only that UOF incidents are higher when 
-# there are more total RTR incidents and UOFshave increased over time
+#--------------
+# Plot all three categories, using color
+# clear the plot window
+dev.off()
 
-
-# Now let's do something similar, but with UOF, SOF and Transitions
-# but here, we'll need to set the range of the x and y
-# first, let's see the min and max of all the data we want
-summary(df)
-
+# generate the first plot
 plot(df$Total_RTR, df$UOF_only, 
      main = "Response to resistance incidents", 
      xlab="Total response to resistance incidents", 
      ylab="Response by type",
-     xlim=c(20,60), ylim=c(0,40),col="red",pch=19)
-points(df$Total_RTR,df$SOF_only,col="blue",pch=19)
-points(df$Total_RTR,df$Transitions,col="green",pch=19)
-# update the legend
-legend("topleft", pch = c(19,19,19), col = c("red","blue","green"), legend = c("UOF", "SOF", "Transitions"))
+     xlim=c(0,60), ylim=c(0,35),
+     col=adjustcolor("red", alpha=0.4),
+     pch=19)
+# Add the other two sets of points
+points(df$Total_RTR,df$SOF_only,
+       col=adjustcolor("blue", alpha=0.4),
+       pch=19)
+points(df$Total_RTR,df$Transitions,
+       col=adjustcolor("green", alpha=0.4),
+       pch=19)
+# add the legend
+legend("topleft", 
+       pch = c(19,19,19), 
+       col = c("red","blue","green"), 
+       legend = c("UOF", "SOF", "Transitions"))
 # Add the regression lines, but color them (par) to match
 abline(lm(df$UOF_only~df$Total_RTR, par(col="red")))
 abline(lm(df$SOF_only~df$Total_RTR, par(col="blue")))
